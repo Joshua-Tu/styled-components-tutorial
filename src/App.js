af8be677de573b434f257b2f59b293314e77dddb
Joshua Tu <joshuatu@Josh-MB.local>
2020-01-08 11:48:24 +1100

    global css added to app css

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     <div>
       <h1>styled components</h1>  
       <h2 style={{color: "red", textTransform: "uppercase"}}>inline styles</h2>
+      <h2 className="global">global css</h2>
     </div>
   );
 }
