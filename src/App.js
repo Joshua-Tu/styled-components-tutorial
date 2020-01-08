77eb0cae8dcdd5ed8e85801e56d660c4b797ce01
Joshua Tu <joshuatu@Josh-MB.local>
2020-01-08 11:57:09 +1100

    css variables added to App.css

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
       <h1>styled components</h1>  
       <h2 style={{color: "red", textTransform: "uppercase"}}>inline styles</h2>
       <h2 className="global">global css</h2>
+      <h2 className="first">first</h2>
+      <h2 className="second">second</h2>
     </div>
   );
 }
