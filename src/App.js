40a15f92711c2f32a6e795aa780fd9ee2956ad6e
Joshua Tu <joshuatu@Josh-MB.local>
2020-01-08 12:14:28 +1100

    deleted Title.css and Title.js, App.js modified, ready for styled-components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import './App.css';
-import Title from './components/Title'
 
-// inline css
-// app css
-// css variables
-// file css
 
 function App() {
   return (
     <div>
-      <h1>styled components</h1>  
-      <h2 style={{color: "red", textTransform: "uppercase"}}>inline styles</h2>
-      <h2 className="global">global css</h2>
-      <h2 className="first">first</h2>
-      <h2 className="second">second</h2>
-      <Title />
+      hello from app
     </div>
   );
 }
