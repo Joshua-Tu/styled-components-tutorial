d608cea8f46fd6764e4e3e26ab47f0405ebd085b
Joshua Tu <joshuatu@Josh-MB.local>
2020-01-08 12:29:34 +1100

    styled components used in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 
+// const name = 'john'
+// const greeting = `hello, my name is ${name}`
+// styled-components utilizes tagged template literals to style your components
+// function`` - tagged template literal
+
+const Button = styled.button`
+  color: red;
+  background: blue;
+  text-transform: uppercase;
+`;
+
+const SecondButton = styled.button`
+  color: blue;
+  background: red;
+  text-transform: uppercase;
+`;
 
 function App() {
   return (
     <div>
       hello from app
+      <Button>click me</Button>
+      <SecondButton>click me i'm second</SecondButton>
     </div>
   );
 }
